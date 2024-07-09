fba303a3b3698b129babd9a29d0fa4aebd6a1cf9
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-07-09 19:49:01 +0000

    f

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+from 0-async_generator import async_generator
+
+async def async_comprehension() -> List[float]:
+    """Collect 10 random numbers using an async comprehension over async_generator."""
+    return [i async for i in async_generator()]
+
